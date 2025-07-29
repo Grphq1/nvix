82cbd13ee39f80b633326c7ea1f8e392df2f8dfc
lorem10 <momenei.alireza1382.15@gmail.com>
2025-07-29 23:32:42 +0330

    feat(ftplugin): add per-language configs for js, ts, vue and react

---
+++ nvim/ftplugin/typescript.lua
@@ -0,0 +1,10 @@
+vim.lsp.start {
+  name = "ts_ls",
+  cmd = { "typescript-language-server", "--stdio" },
+  root_dir = vim.fs.dirname(vim.fs.find({
+    "tsconfig.json",
+    "package.json",
+    "jsconfig.json",
+  }, { upward = true })[1]),
+}
+
---
+++ nvim/ftplugin/vue.lua
@@ -0,0 +1,25 @@
+vim.lsp.start {
+  name = "ts_ls",
+  cmd = { "typescript-language-server", "--stdio" },
+  init_options = {
+    plugins = { {
+      name = "@vue/typescript-plugin",
+      location = vim.env.VUE_TYPESCRIPT_PLUGIN,
+      languages = { "vue" }
+    } }
+  }
+}
+
+vim.lsp.start {
+  name = "volar",
+  cmd = { "vue-language-server", "--stdio" },
+  filetypes = { "vue", "javascript", "typescript", "javascriptreact", "typescriptreact" },
+  init_options = {
+    vue = {
+      hybridMode = false
+    },
+    typescript = {
+      tsdk = vim.env.VUE_TSDK
+    }
+  }
+}
