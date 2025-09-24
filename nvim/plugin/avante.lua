8bb3f8c511aaceb93d517e9be50b11530f5ffee6
lorem10 <momenei.alireza1382.15@gmail.com>
2025-09-24 19:10:17 +0330

    fix: no provider found: mistral for now

--- nvim/plugin/avante.lua
+++ nvim/plugin/avante.lua
@@ -1,39 +1,39 @@
-if vim.g.did_load_avante_plugin then
-  return
-end
-vim.g.did_load_avante_plugin = true
-
-require('avante_lib').load()
-require('avante').setup {
-  debug = false,
-  provider = 'mistral',
-  auto_suggestions_provider = 'mistral',
-
-  providers = {
-    openai = {
-      endpoint = 'https://api.avalapis.ir/v1',
-      model = 'o4-mini',
-    },
-
-    mistral = {
-      __inherited_from = 'openai',
-      api_key_name = 'MISTRAL_API_KEY',
-      endpoint = 'https://api.mistral.ai/v1',
-      model = 'mistral-large-latest',
-      extra_request_body = {
-        max_tokens = 2000,
-      },
-    },
-  },
-
-  highlights = {
-    diff = {
-      current = 'DiffText',
-      incoming = 'DiffAdd',
-    },
-  },
-  windows = {
-    position = 'right',
-    width = 30,
-  },
-}
+-- if vim.g.did_load_avante_plugin then
+--   return
+-- end
+-- vim.g.did_load_avante_plugin = true
+--
+-- require('avante_lib').load()
+-- require('avante').setup {
+--   debug = false,
+--   provider = 'mistral',
+--   auto_suggestions_provider = 'mistral',
+--
+--   providers = {
+--     openai = {
+--       endpoint = 'https://api.avalapis.ir/v1',
+--       model = 'o4-mini',
+--     },
+--
+--     mistral = {
+--       __inherited_from = 'openai',
+--       api_key_name = 'MISTRAL_API_KEY',
+--       endpoint = 'https://api.mistral.ai/v1',
+--       model = 'mistral-large-latest',
+--       extra_request_body = {
+--         max_tokens = 2000,
+--       },
+--     },
+--   },
+--
+--   highlights = {
+--     diff = {
+--       current = 'DiffText',
+--       incoming = 'DiffAdd',
+--     },
+--   },
+--   windows = {
+--     position = 'right',
+--     width = 30,
+--   },
+-- }
