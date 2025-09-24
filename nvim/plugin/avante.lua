57cf9b1b4d3232ce8e774ede0525b5b9468dfa68
lorem10 <momenei.alireza1382.15@gmail.com>
2025-09-24 19:01:11 +0330

    feat: setup mistral provider

--- nvim/plugin/avante.lua
+++ nvim/plugin/avante.lua
@@ -6,21 +6,24 @@ vim.g.did_load_avante_plugin = true
 require('avante_lib').load()
 require('avante').setup {
   debug = false,
-  provider = 'claude',
-  auto_suggestions_provider = 'openai',
+  provider = 'mistral_codestral',
+  auto_suggestions_provider = 'mistral_codestral',
 
-  openai = {
-    endpoint = 'https://api.avalapis.ir/v1',
-    temperature = 0.7,
-    model = 'o4-mini',
-    max_tokens = 4096,
-  },
+  providers = {
+    openai = {
+      endpoint = 'https://api.avalapis.ir/v1',
+      model = 'o4-mini',
+    },
 
-  claude = {
-    endpoint = 'https://api.avalapis.ir',
-    model = 'anthropic.claude-3-7-sonnet-20250219-v1:0',
-    temperature = 0.7,
-    max_tokens = 4096,
+    mistral_codestral = {
+      __inherited_from = 'openai',
+      api_key_name = 'MISTRAL_API_KEY',
+      endpoint = 'https://api.mistral.ai/v1',
+      model = 'mistral-large-latest',
+      extra_request_body = {
+        max_tokens = 2000,
+      },
+    },
   },
 
   highlights = {
