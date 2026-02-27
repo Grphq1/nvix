3baf2aa34dab12b0efc4632cdb10b52e726d4a85
lorem10 <momenei.alireza1382.15@gmail.com>
2026-02-27 15:50:56 +0330

    feat: add diagnostics at tabline

--- nvim/plugin/tabline.lua
+++ nvim/plugin/tabline.lua
@@ -9,6 +9,12 @@ local render = function(f)
   f.add ' '
 
   local cur_buf = vim.api.nvim_get_current_buf()
+  local severity = vim.diagnostic.severity
+
+  local function diagnostics_count(bufnr, level)
+    return #vim.diagnostic.get(bufnr, { severity = level })
+  end
+
   f.make_bufs(function(info)
     local mark = info.buf == cur_buf and '*' or ' '
     f.add(' ' .. mark .. ' ' .. info.buf .. ' ')
@@ -24,6 +30,16 @@ local render = function(f)
 
     f.add ' '
   end)
+
+  local errors = diagnostics_count(cur_buf, severity.ERROR)
+  local warns = diagnostics_count(cur_buf, severity.WARN)
+  local infos = diagnostics_count(cur_buf, severity.INFO)
+  local hints = diagnostics_count(cur_buf, severity.HINT)
+
+  if errors + warns + infos + hints > 0 then
+    f.add '%='
+    f.add('E:' .. errors .. ' W:' .. warns .. ' I:' .. infos .. ' H:' .. hints .. ' ')
+  end
 end
 
 vim.api.nvim_set_hl(0, 'TabLineFill', { bg = 'NONE' })
