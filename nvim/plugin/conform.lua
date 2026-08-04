c63be1a336f3125e56f51e92a1d538b85804bdc7
lorem10 <momenei.alireza1382.15@gmail.com>
2026-08-04 21:32:13 +0330

    feat(conform): add oxfmt as primary formatter with prettier fallback

--- nvim/plugin/conform.lua
+++ nvim/plugin/conform.lua
@@ -1,21 +1,26 @@
--- nvim/lua/user/conform.lua
 local conform = require('conform')
 
 conform.setup {
   formatters_by_ft = {
-    javascript = { 'prettier' },
-    typescript = { 'prettier' },
-    vue = { 'prettier' },
-    javascriptreact = { 'prettier' },
-    typescriptreact = { 'prettier' },
-    css = { 'prettier' },
-    scss = { 'prettier' },
+    javascript = { 'oxfmt', 'prettier', stop_after_first = true },
+    javascriptreact = { 'oxfmt', 'prettier', stop_after_first = true },
+    typescript = { 'oxfmt', 'prettier', stop_after_first = true },
+    typescriptreact = { 'oxfmt', 'prettier', stop_after_first = true },
+    vue = { 'oxfmt', 'prettier' },
+
+    css = { 'oxfmt', 'prettier', stop_after_first = true },
+    scss = { 'oxfmt', 'prettier', stop_after_first = true },
     markdown = { 'prettier' },
+
     lua = { 'stylua' },
     nix = { 'alejandra' },
   },
 }
 
 vim.keymap.set('n', '<space>f', function()
-  conform.format { async = true }
-end, { desc = 'Format buffer with Prettier' })
+  conform.format {
+    async = true,
+  }
+end, {
+  desc = 'Format buffer',
+})
