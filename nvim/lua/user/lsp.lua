8fffa2db735ed126cd2dda8fa701d24ab9633ff5
lorem10 <momenei.alireza1382.15@gmail.com>
2026-08-04 21:31:31 +0330

    refactor(lsp): unify find_root_dir with nil_ok param

--- nvim/lua/user/lsp.lua
+++ nvim/lua/user/lsp.lua
@@ -6,26 +6,38 @@
 
 local M = {}
 
----Finds the root directory for a project by looking for specific files
----@param files table List of files to search for (e.g., {'package.json', 'tsconfig.json'})
----@return string The root directory path, falls back to current working directory
-function M.find_root_dir(files)
-  local root_files = vim.fs.find(files, { upward = true })
-  return root_files[1] and vim.fs.dirname(root_files[1]) or vim.fn.getcwd()
+---@param files string[]
+---@param path? string
+---@param nil_ok? boolean
+---@return string?
+function M.find_root_dir(files, path, nil_ok)
+  path = path or vim.fn.getcwd()
+
+  local root_files = vim.fs.find(files, {
+    path = path,
+    upward = true,
+  })
+
+  local dir = root_files[1] and vim.fs.dirname(root_files[1])
+  if dir then
+    return dir
+  end
+  if nil_ok then
+    return nil
+  end
+  return vim.fn.getcwd()
 end
 
----Gets a 'ClientCapabilities' object, describing the LSP client capabilities
----Extends the object with capabilities provided by plugins.
 ---@diagnostic disable-next-line: undefined-doc-name
 ---@return lsp.ClientCapabilities
 function M.make_client_capabilities()
   local capabilities = vim.lsp.protocol.make_client_capabilities()
-  -- Add com_nvim_lsp capabilities
+
   local cmp_lsp = require('cmp_nvim_lsp')
   local cmp_lsp_capabilities = cmp_lsp.default_capabilities()
+
   capabilities = vim.tbl_deep_extend('keep', capabilities, cmp_lsp_capabilities)
-  -- Add any additional plugin capabilities here.
-  -- Make sure to follow the instructions provided in the plugin's docs.
+
   return capabilities
 end
 
