1974ae79e68c9f188cd6860f80f8901bde1b141b
lorem10 <momenei.alireza1382.15@gmail.com>
2026-09-20 12:44:42 +0330

    chore: clean up my keymaps

--- nvim/plugin/keymaps.lua
+++ nvim/plugin/keymaps.lua
@@ -8,93 +8,27 @@ local fn = vim.fn
 local keymap = vim.keymap
 local diagnostic = vim.diagnostic
 
--- Yank from current position till end of current line
-keymap.set('n', 'Y', 'y$', { silent = true, desc = '[Y]ank to end of line' })
-
 -- Toggle the quickfix list (only opens if it is populated)
-local function toggle_qf_list()
-  local qf_exists = false
-  for _, win in pairs(fn.getwininfo() or {}) do
-    if win['quickfix'] == 1 then
-      qf_exists = true
-    end
-  end
-  if qf_exists == true then
-    vim.cmd.cclose()
+local function toggle_diagnostics()
+  local loclist_win = vim.fn.getloclist(0, { winid = 0 }).winid
+  if loclist_win ~= 0 then
+    vim.cmd.lclose()
     return
   end
-  if not vim.tbl_isempty(vim.fn.getqflist()) then
-    vim.cmd.copen()
-  end
+
+  vim.diagnostic.setloclist({
+    open = true,
+    title = 'Diagnostics',
+    format = function()
+      local source = diagnostic.source and ('[' .. diagnostic.source .. '] ') or ''
+      local code = diagnostic.code and ('(' .. diagnostic.code .. ') ') or ''
+      return source .. code .. diagnostic.message
+    end,
+  })
 end
 
-keymap.set('n', '<C-c>', toggle_qf_list, { desc = 'toggle quickfix list' })
+vim.keymap.set('n', '<C-c>', toggle_diagnostics, { desc = 'Toggle diagnostics (loclist)' })
 
-local function try_fallback_notify(opts)
-  local success, _ = pcall(opts.try)
-  if success then
-    return
-  end
-  success, _ = pcall(opts.fallback)
-  if success then
-    return
-  end
-  vim.notify(opts.notify, vim.log.levels.INFO)
-end
-
--- Cycle the quickfix and location lists
-local function cleft()
-  try_fallback_notify {
-    try = vim.cmd.cprev,
-    fallback = vim.cmd.clast,
-    notify = 'Quickfix list is empty!',
-  }
-end
-
-local function cright()
-  try_fallback_notify {
-    try = vim.cmd.cnext,
-    fallback = vim.cmd.cfirst,
-    notify = 'Quickfix list is empty!',
-  }
-end
-
-keymap.set('n', '[c', cleft, { silent = true, desc = '[c]ycle quickfix left' })
-keymap.set('n', ']c', cright, { silent = true, desc = '[c]ycle quickfix right' })
-keymap.set('n', '[C', vim.cmd.cfirst, { silent = true, desc = 'first quickfix entry' })
-keymap.set('n', ']C', vim.cmd.clast, { silent = true, desc = 'last quickfix entry' })
-
-local function lleft()
-  try_fallback_notify {
-    try = vim.cmd.lprev,
-    fallback = vim.cmd.llast,
-    notify = 'Location list is empty!',
-  }
-end
-
-local function lright()
-  try_fallback_notify {
-    try = vim.cmd.lnext,
-    fallback = vim.cmd.lfirst,
-    notify = 'Location list is empty!',
-  }
-end
-
-keymap.set('n', '[l', lleft, { silent = true, desc = 'cycle [l]oclist left' })
-keymap.set('n', ']l', lright, { silent = true, desc = 'cycle [l]oclist right' })
-keymap.set('n', '[L', vim.cmd.lfirst, { silent = true, desc = 'first [L]oclist entry' })
-keymap.set('n', ']L', vim.cmd.llast, { silent = true, desc = 'last [L]oclist entry' })
-
--- Resize vertical splits
-local toIntegral = math.ceil
--- Close floating windows [Neovim 0.10 and above]
-keymap.set('n', '<leader>fq', function()
-  vim.cmd('fclose!')
-end, { silent = true, desc = '[f]loating windows: [q]uit/close all' })
-
--- Remap Esc to switch to normal mode and Ctrl-Esc to pass Esc to terminal
-keymap.set('t', '<Esc>', '<C-\\><C-n>', { desc = 'switch to normal mode' })
-keymap.set('t', '<C-Esc>', '<Esc>', { desc = 'send Esc to terminal' })
 
 -- Shortcut for expanding to current buffer's directory in command mode
 keymap.set('c', '%%', function()
@@ -104,9 +38,6 @@ keymap.set('c', '%%', function()
     return '%%'
   end
 end, { expr = true, desc = "expand to current buffer's directory" })
-
-keymap.set('n', '<space>tn', vim.cmd.tabnew, { desc = '[t]ab: [n]ew' })
-keymap.set('n', '<space>tq', vim.cmd.tabclose, { desc = '[t]ab: [q]uit/close' })
 
 local severity = diagnostic.severity
 
@@ -170,15 +101,4 @@ keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'move [u]p half-page and center' })
 keymap.set('n', '<C-f>', '<C-f>zz', { desc = 'move DOWN [f]ull-page and center' })
 keymap.set('n', '<C-b>', '<C-b>zz', { desc = 'move UP full-page and center' })
 
---- Disabled keymaps [enable at your own risk]
-
--- Automatic management of search highlight
--- XXX: This is not so nice if you use j/k for navigation
--- (you should be using <C-d>/<C-u> and relative line numbers instead ;)
---
--- local auto_hlsearch_namespace = vim.api.nvim_create_namespace('auto_hlsearch')
--- vim.on_key(function(char)
---   if vim.fn.mode() == 'n' then
---     vim.opt.hlsearch = vim.tbl_contains({ '<CR>', 'n', 'N', '*', '#', '?', '/' }, vim.fn.keytrans(char))
---   end
--- end, auto_hlsearch_namespace)
+keymap.set('n', '<leader>pv', vim.cmd.Ex)
